55ce30993505378f639c21ba49fadacabffabebc
Marcos Teodoro <marcosvinici_us@live.com>
2021-03-27 18:21:04 -0300

    Implementação dos estados com redux direto no componente CounterContainer

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -2,24 +2,39 @@
 
 import React, { PureComponent } from 'react'
 import Counter from './counter'
+import { createStore } from 'redux'
+
+const counter = (state = 0, action) => {
+  switch (action.type) {
+    case 'DECREMENT': return state - 1
+    case 'INCREMENT': return state + 1
+  }
+
+  return state
+}
+
+const store = createStore(counter)
 
 class CounterContainer extends PureComponent {
   constructor () {
     super()
 
-    this.state = {
-      counter: 0
-    }
+    this.increment = () => store.dispatch({ type: 'INCREMENT' })
+    this.decrement = () => store.dispatch({ type: 'DECREMENT' })
+  }
 
-    this.increment = () => this.setState({ counter: this.state.counter + 1 })
-    this.decrement = () => this.setState({ counter: this.state.counter - 1 })
+  componentDidMount () {
+    store.subscribe(() => {
+      this.setState({
+        counter: this.forceUpdate()
+      })
+    })
   }
 
   render () {
-    const { counter } = this.state
     return (
       <Counter
-        counter={counter}
+        counter={store.getState()}
         decrement={this.decrement}
         increment={this.increment}
       />
