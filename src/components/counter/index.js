ede68f81ae0f173b10c64f32ff50f4f1c8c1d1a6
Marcos Teodoro <marcosvinici_us@live.com>
2021-03-27 18:31:10 -0300

    Extração da store para index para reutilização na aplicação

--- src/components/counter/index.js
+++ src/components/counter/index.js
@@ -2,39 +2,31 @@
 
 import React, { PureComponent } from 'react'
 import Counter from './counter'
-import { createStore } from 'redux'
-
-const counter = (state = 0, action) => {
-  switch (action.type) {
-    case 'DECREMENT': return state - 1
-    case 'INCREMENT': return state + 1
-  }
-
-  return state
-}
-
-const store = createStore(counter)
 
 class CounterContainer extends PureComponent {
   constructor () {
     super()
 
-    this.increment = () => store.dispatch({ type: 'INCREMENT' })
-    this.decrement = () => store.dispatch({ type: 'DECREMENT' })
+    this.increment = () => this.props.store.dispatch({ type: 'INCREMENT' })
+    this.decrement = () => this.props.store.dispatch({ type: 'DECREMENT' })
   }
 
   componentDidMount () {
-    store.subscribe(() => {
+    this.unsubscribe = this.props.store.subscribe(() => {
       this.setState({
         counter: this.forceUpdate()
       })
     })
   }
 
+  componentWillUnmount () {
+    this.unsubscribe()
+  }
+
   render () {
     return (
       <Counter
-        counter={store.getState()}
+        counter={this.props.store.getState()}
         decrement={this.decrement}
         increment={this.increment}
       />
